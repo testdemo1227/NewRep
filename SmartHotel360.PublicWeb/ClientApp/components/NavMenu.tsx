6dc650a270fd07c11fe4743b15386a16c7f535e0
PLAINCONCEPTS\vbugarin <vbugarin@plainconcepts.com>
2017-09-22 15:38:40 +0200

    hide button to collapse menu

--- SmartHotel360.PublicWeb/ClientApp/components/NavMenu.tsx
+++ SmartHotel360.PublicWeb/ClientApp/components/NavMenu.tsx
@@ -12,7 +12,7 @@ export class NavMenu extends React.Component<any, {}> {
         return <div className={`sh-navMenu ${this.isHome() ? 'sh-navMenu--home' : ''}`}>
             <div className={`sh-navMenu-wrapper`}>
                 <div className='navbar-header sh-navMenu-header'>
-                    <button type='button' className='sh-navMenu-toogle navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
+                    <button type='button' className='sh-navMenu-toogle navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse' hidden>
                         <span className='sr-only'>Toggle navigation</span>
                         <span className='icon-bar'></span>
                         <span className='icon-bar'></span>
@@ -20,29 +20,30 @@ export class NavMenu extends React.Component<any, {}> {
                     </button>
                     <Link className='navbar-brand' to={'/'}>SmartHotel360.PublicWeb</Link>
                 </div>
-
-                <ul className='nav navbar-nav sh-navMenu-nav hidden-sm-down'>
-                    <li>
-                        <NavLink exact to={'/smartExperience'} activeClassName='active'>
-                            The smart experience
+                <div className='navbar-collapse collapse'>
+                    <ul className='nav navbar-nav sh-navMenu-nav'>
+                        <li>
+                            <NavLink exact to={'/smartExperience'} activeClassName='active'>
+                                The smart experience
                             </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={'/rooms'} activeClassName='active'>
-                            Rooms
+                        </li>
+                        <li>
+                            <NavLink to={'/rooms'} activeClassName='active'>
+                                Rooms
                             </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={'/conferenceRooms'} activeClassName='active'>
-                            Conference rooms
+                        </li>
+                        <li>
+                            <NavLink to={'/conferenceRooms'} activeClassName='active'>
+                                Conference rooms
                             </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={'/login'} activeClassName='active'>
-                            Login
+                        </li>
+                        <li>
+                            <NavLink to={'/login'} activeClassName='active'>
+                                Login
                             </NavLink>
-                    </li>
-                </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>;
     }
