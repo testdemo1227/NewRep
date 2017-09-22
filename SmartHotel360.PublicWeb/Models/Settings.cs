baea146ef960bb8208cd4c1216c0756bc21b76ef
Quique Fdez Guerra <efernandez@plainconcepts.com>
2017-09-22 15:01:18 +0200

    Add settings

---
+++ SmartHotel360.PublicWeb/Models/Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartHotel360.PublicWeb.Models
+{
+    public class Settings
+    {
+        public bool Production { get; set; }
+        public string BookingsUrl { get; set; }
+        public string HotelsUrl { get; set; }
+        public string SuggestionsUrl { get; set; }
+    }
+}
