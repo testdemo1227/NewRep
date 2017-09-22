2f3e5dd7ae84954559abd4a8e0b77122f8a39444
Oleksandr Kryzhanovskyy <okryz@plainconcepts.com>
2017-09-22 12:55:56 +0200

    finished mark up added images

--- SmartHotel360.PublicWeb/ClientApp/components/Footer.tsx
+++ SmartHotel360.PublicWeb/ClientApp/components/Footer.tsx
@@ -3,8 +3,66 @@ import { NavLink, Link } from 'react-router-dom';
 
 export class Footer extends React.Component<{}, {}> {
     public render() {
-        return <div>
-            Footer
+        return <div className='sh-footer-content'>
+            <div className='sh-footer-section'>Logo</div>
+            <div>
+                <div className='sh-footer-section'>
+                    <ul className='sh-footer-list'>
+                        <li>
+                            <a className='sh-footer-link' href='#'>The smart experience</a>
+                        </li>
+                        <li>
+                            <a className='sh-footer-link' href='#'>Cities</a>
+                        </li>
+                        <li>
+                            <a className='sh-footer-link' href='#'>Rooms</a>
+                        </li>
+                        <li>
+                            <a className='sh-footer-link' href='#'>Conference rooms</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <div className='sh-footer-section'>
+                    <ul className='sh-footer-list'>
+                        <li>
+                            <a className='sh-footer-link' href='#'>Terms & Conditions</a>
+                        </li>
+                        <li>
+                            <a className='sh-footer-link' href='#'>Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a className='sh-footer-link' href='#'>Help</a>
+                        </li>
+                        <li>
+                            <a className='sh-footer-link' href='#'>Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <div className='sh-footer-section'>
+                    <p>Share</p>
+                    <ul className='sh-footer-social'>
+                        <li>
+                            <a href='#'>
+                                <img className="sh-footer-social_icon" src='/assets/images/instagram.svg' />
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#'>
+                                <img className="sh-footer-social_icon" src='/assets/images/facebook.svg' />
+                            </a>
+                        </li>
+                        <li>
+                            <a href='#'>
+                                <img className="sh-footer-social_icon" src='/assets/images/twitter.svg' />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>;
     }
 }
