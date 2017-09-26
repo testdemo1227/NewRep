e12d533e99eb91bac90a75578696af1996abdf54
PLAINCONCEPTS\vbugarin <vbugarin@plainconcepts.com>
2017-09-26 18:32:51 +0200

    Layout for smart experience info. Mobile version. Add icons for smart experience.

--- SmartHotel360.PublicWeb/ClientApp/components/Home.tsx
+++ SmartHotel360.PublicWeb/ClientApp/components/Home.tsx
@@ -23,6 +23,56 @@ export default class Home extends React.Component<RouteComponentProps<{}>, {}> {
                     </ul>
                 </div>
             </div>
+            <section className='sh-home-info'>
+                <p className='sh-home-info-title'>The smart experience</p>
+                <article className='sh-home-info-row'>
+                    <div className='sh-home-info-icon'>
+                        <img src='/assets/images/ic_check.svg' />
+                    </div>
+                    <div className='sh-home-info-column'>
+                        <p className='sh-home-info-subtitle'>Check in from your phone</p>
+                        <span className='sh-home-info-text'>
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley. 
+                        </span>
+                    </div>
+                </article>
+
+                <article className='sh-home-info-row'>
+                    <div className='sh-home-info-icon'>
+                        <img src='/assets/images/ic_personalize.svg' />
+                    </div>
+                    <div className='sh-home-info-column'>
+                        <p className='sh-home-info-subtitle'>Personalize your experience</p>
+                        <span className='sh-home-info-text'>
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.
+                        </span>
+                    </div>
+                </article>
+
+                <article className='sh-home-info-row'>
+                    <div className='sh-home-info-icon'>
+                        <img src='/assets/images/ic_padlock.svg' />
+                    </div>
+                    <div className='sh-home-info-column'>
+                        <p className='sh-home-info-subtitle'>Find and access your room</p>
+                        <span className='sh-home-info-text'>
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.
+                        </span>
+                    </div>
+                </article>
+
+                <article className='sh-home-info-row'>
+                    <div className='sh-home-info-icon'>
+                        <img src='/assets/images/ic_begreen.svg' />
+                    </div>
+                    <div className='sh-home-info-column'>
+                        <p className='sh-home-info-subtitle'>Be green</p>
+                        <span className='sh-home-info-text'>
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.
+                        </span>
+                    </div>
+                </article>
+            </section>
         </div>;
     }
 }
