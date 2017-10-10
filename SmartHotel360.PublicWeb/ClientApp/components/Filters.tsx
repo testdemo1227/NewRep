1332d0050d8928d2d137c319bb33b1da66b21573
enriquefegu <enriquefegu@outlook.com>
2017-10-10 16:16:01 +0000

    Merged PR 178: Add map icon
    Add map icon
    
    Related work items: #1134

--- SmartHotel360.PublicWeb/ClientApp/components/Filters.tsx
+++ SmartHotel360.PublicWeb/ClientApp/components/Filters.tsx
@@ -14,26 +14,32 @@ export default class Filters extends React.Component<{}, {}> {
 
     public render() {
         return <div className='sh-filters'>
-            <FilterAvailability />
-            <FilterRating rating={3} />
-            <Filter title='Price range'>
-                <FilterPrice />
-            </Filter>
-            <Filter title='Reviews' disabled={true}>
-                <FilterReviews />
-            </Filter>
-            <Filter title='Services'>
-                <FilterServices />
-            </Filter>
-            <Filter title='Cancelation' disabled={true}>
-                <FilterCancelation />
-            </Filter>
-            <Filter title='Neighborhood' right={0} disabled={true}>
-                <FilterNeighborhood />
-            </Filter>
-            <Filter title='Order by relevance' right={0} disabled={true}>
-                <FilterRelevance />
-            </Filter>
+            <div className='sh-filters-container'>
+                <FilterAvailability />
+                <FilterRating rating={3} />
+                <Filter title='Price range'>
+                    <FilterPrice />
+                </Filter>
+                <Filter title='Reviews' disabled={true}>
+                    <FilterReviews />
+                </Filter>
+                <Filter title='Services'>
+                    <FilterServices />
+                </Filter>
+                <Filter title='Cancelation' disabled={true}>
+                    <FilterCancelation />
+                </Filter>
+                <Filter title='Neighborhood' right={0} disabled={true}>
+                    <FilterNeighborhood />
+                </Filter>
+                <Filter title='Order by relevance' right={0} disabled={true}>
+                    <FilterRelevance />
+                </Filter>
+            </div>
+
+            <div className='sh-filters-float'>
+                <i className='sh-filters-icon icon-sh-map'></i>
+            </div>
         </div>;
     }
 }
